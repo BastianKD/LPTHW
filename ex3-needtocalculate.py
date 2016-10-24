433ef68e5142df351658761bde19bf6d3485a919
BKD <bkdelarocque@gmail.com>
2016-10-24 16:40:28 +0200

    Add files via upload

---
+++ ex3-needtocalculate.py
@@ -0,0 +1,18 @@
+print "Alright dude, enough is enough, now it's your turn to do some math for me!"
+print "Huh okay I guess..."
+print "I didn't ask!"
+print "When was your Grandmother born?"
+print "1931 I guess!"
+print "You only guess? What a nice Grandson you are... Anyway, now how old is she then?"
+print 2016 - 1931
+print "Okay it's right I guess..."
+print "calculating..."
+print "calculating..."
+print "calculating..."
+print "calculating..."
+print 2016 - 1931
+print "Haha you're so lame dude!"
+print "Mmpfhh and what about you tell me how much 5 * 7 / 2 * 4 + 3 is?"
+print "Easy dude, got it!", 5 * 7 / 2 * 4 + 3
+print "Thanks!"
+print "See ya."
