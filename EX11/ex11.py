0ade65bd774e30c7c45909b161dc37c9eede7ddb
Bastian_KD <bkdelarocque@gmail.com>
2016-10-25 12:55:30 +0200

    new fils in folders

---
+++ EX11/ex11.py
@@ -0,0 +1,19 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+
+
+print "What's your favorite film?",
+fav_film = raw_input()
+print "What's your second name?",
+sec_name = raw_input()
+print "What are you doing for a living?",
+do_liv = raw_input()
+
+print "Woaw that doesn't make any sense!"
